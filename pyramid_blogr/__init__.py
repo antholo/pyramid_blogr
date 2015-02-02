4687377dc52d3a8120eb75a66d3afb105d40b001
Brian <blooker@gmail.com>
2015-02-02 07:45:53 -0600

    initial commit

---
+++ pyramid_blogr/__init__.py
@@ -0,0 +1,26 @@
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from .security import EntryFactory
+from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+
+from .models import DBSession
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    authentication_policy = AuthTktAuthenticationPolicy('somesecret')
+    authorization_policy = ACLAuthorizationPolicy()
+    config = Configurator(settings=settings,
+                          authentication_policy=authentication_policy,
+                          authorization_policy=authorization_policy
+                          )
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.add_route('blog', '/blog/{id:\d+}/{slug}')
+    config.add_route('blog_action', '/blog/{action}')
+    config.add_route('auth', '/sign/{action}')
+    config.scan()
+    return config.make_wsgi_app()
